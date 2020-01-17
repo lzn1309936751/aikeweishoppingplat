3cb0109bf8c911204962e971d5879f6d32a4c3df
lzn <1309936751@qq.com>
2020-01-17 16:04:54 +0800

    aikeweishoppingplat

--- shoppingPlat-dao/src/main/java/com/dao/CouponDao.java
+++
@@ -1,8 +0,0 @@
-package com.dao;
-
-import com.entity.CouponEntity;
-import org.apache.ibatis.annotations.Param;
-
-public interface CouponDao {
-    CouponEntity getByMoney(@Param("total") double total);
-}
